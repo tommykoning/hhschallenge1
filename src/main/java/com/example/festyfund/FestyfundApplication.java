6ed8d17429fcb6adfd2fc7e1136904eb186ca505
tommy <tommykoning@live.nl>
2021-11-10 12:04:09 +0100

    Update FestyfundApplication.java, application.properties, and FestyfundApplicationTests.java

---
+++ src/main/java/com/example/festyfund/FestyfundApplication.java
@@ -0,0 +1,13 @@
+package com.example.festyfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestyfundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestyfundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/festyfund/FestyfundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.festyfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestyfundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
